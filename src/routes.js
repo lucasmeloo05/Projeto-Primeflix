896018165b05b0b4a687cc812b404bcd69f1bcb1
Lucas Melo <lucasmelojedi@gmail.com>
2024-11-19 03:12:16 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Filme from './pages/Filmes';
+import Erro from './pages/Erro';
+import Favoritos from './pages/Favoritos';
+
+import Header from './components/Header';
+
+function RoutesApp(){
+    return(
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/filme/:id' element={<Filme/>}/>
+                <Route path='/favoritos' element={<Favoritos/>}/>
+
+                <Route path='*' element={<Erro/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RoutesApp;
